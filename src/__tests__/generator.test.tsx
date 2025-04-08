e9ddb80fbf9054c92a0f7157e494559c74b62610
Boris Lebon <bo.lebon@gmail.com>
2025-04-08 17:59:21 +0200

    fix: windows compatibility

--- src/__tests__/generator.test.tsx
+++ src/__tests__/generator.test.tsx
@@ -18,23 +18,53 @@ describe("Generator", () => {
   it("should generare the container file based on the source directory", async () => {
     const generator = new ContainerGenerator(
       [
-        path.resolve(DUMMY_SRC_DIR, "./*.ts"),
-        path.resolve(DUMMY_SRC_DIR, "./*.tsx"),
+        path.resolve(DUMMY_SRC_DIR, "./*.ts").split(path.sep).join('/'),
+        path.resolve(DUMMY_SRC_DIR, "./*.tsx").split(path.sep).join('/'),
       ],
       [],
       TS_CONFIG_PATH,
       CONTAINER_PATH
     );
 
-    await generator.generate(path.relative(DUMMY_SRC_DIR, EYEOSEE_MODULE_PATH));
+    await generator.generate(path.relative(DUMMY_SRC_DIR, EYEOSEE_MODULE_PATH).split(path.sep).join('/'));
 
     const containerFileContent = (await fs.readFile(CONTAINER_PATH)).toString();
 
     expect(containerFileContent).toEqual(
+      // Check that there is a ContainerDependencies type and that it's exported
       expect.stringMatching(
-        /export\s+type\s+ContainerDependencies\s*=\s*{\s*useDecoratedMessage:\s*ExtractedContainerItem<\s*typeof\s+useDecoratedMessage_(\d+)\s*>;\s*decorateMessage:\s*ExtractedContainerItem<\s*typeof\s+decorateMessage_(\d+)\s*>;\s*CONFIG:\s*ExtractedContainerItem<\s*typeof\s+config_(\d+)\s*>;\s*MessageDecorator:\s*ExtractedContainerItem<\s*typeof\s+default_(\d+)\s*>;\s*};\s*export\s+const\s+container\s*=\s*new\s+Container<\s*ContainerDependencies\s*>\(\s*\);/
+        /export\s+type\s+ContainerDependencies\s*=\s*{.*};\s*export\s+const\s+container\s*=\s*new\s+Container<\s*ContainerDependencies\s*>\(\s*\);/s
       )
     );
+
+    // Check that there is a useDecoratedMessage dependency
+    expect(containerFileContent).toEqual(
+      expect.stringMatching(
+        /export\s+type\s+ContainerDependencies\s*=\s*{.*useDecoratedMessage:\s*ExtractedContainerItem<\s*typeof\s+useDecoratedMessage_(\d+)\s*>.*}/s
+      )
+    );
+
+    // Check that there is a MessageDecorator dependency
+    expect(containerFileContent).toEqual(
+      expect.stringMatching(
+        /export\s+type\s+ContainerDependencies\s*=\s*{.*MessageDecorator:\s*ExtractedContainerItem<\s*typeof\s+default_(\d+)\s*>.*}/s
+      )
+    );
+
+    // Check that there is a decorateMessage dependency
+    expect(containerFileContent).toEqual(
+      expect.stringMatching(
+        /export\s+type\s+ContainerDependencies\s*=\s*{.*decorateMessage:\s*ExtractedContainerItem<\s*typeof\s+decorateMessage_(\d+)\s*>.*}/s
+      )
+    );
+
+    // Check that there is a CONFIG dependency
+    expect(containerFileContent).toEqual(
+      expect.stringMatching(
+        /export\s+type\s+ContainerDependencies\s*=\s*{.*CONFIG:\s*ExtractedContainerItem<\s*typeof\s+config_(\d+)\s*>.*}/s
+      )
+    );
+
 
     const { container, initContainer, ContainerInitializer } = await import(
       CONTAINER_PATH
